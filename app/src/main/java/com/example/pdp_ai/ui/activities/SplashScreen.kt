1b20eea8ee0048716238778670686004357ff880
OgabekDev <ogabekdev@gmail.com>
2021-12-13 14:48:43 +0500

    New Update v1.0.2

--- app/src/main/java/com/example/pdp_ai/ui/activities/SplashScreen.kt
+++ app/src/main/java/com/example/pdp_ai/ui/activities/SplashScreen.kt
@@ -1,10 +1,13 @@
 package com.example.pdp_ai.ui.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
 import com.example.pdp_ai.R
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
@@ -19,12 +22,38 @@ class SplashScreen : AppCompatActivity() {
         imageView.startAnimation(bottom)
         textView.startAnimation(top)
 
-        val splashTime = 2600
-        Handler().postDelayed({
-            startActivity(Intent(this, ChatScreen::class.java))
-            finish()
+        if (true) {
 
-        }, splashTime.toLong())
+        } else {
+            val alertDialog = AlertDialog.Builder(this).create()
+            alertDialog.setTitle("Internet Connection Error")
+            alertDialog.setMessage("Server bilan aloqa yo'q. Iltimos internetni tekshiring va dasturni qayta ishga tushuring")
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") {
+                    _: DialogInterface, _: Int -> finish()
+            }
+            alertDialog.show()
+        }
+
+
+//        var requestCount = 0
+//        while (true) {
+//            if () {
+//                break
+//            } else {
+//                requestCount++
+//            }
+//            if (requestCount >= 3) {
+//
+//            }
+//        }
+
+
+//        val splashTime = 2600
+//        Handler().postDelayed({
+//            startActivity(Intent(this, ChatScreen::class.java))
+//            finish()
+//
+//        }, splashTime.toLong())
 
     }
 }
